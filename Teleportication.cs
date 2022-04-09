862d9c20b25753355889eef9ed14643a7ccc4e5e
Remod <desolationoutpostpve@gmail.com>
2022-04-09 07:34:22 -0500

    Added inbound hooks for set, remove, and query of server tp targets

--- Teleportication.cs
+++ Teleportication.cs
@@ -43,7 +43,7 @@ using UnityEngine;
 // Economics for bypass
 namespace Oxide.Plugins
 {
-    [Info("Teleportication", "RFC1920", "1.3.0")]
+    [Info("Teleportication", "RFC1920", "1.3.1")]
     [Description("NextGen Teleportation plugin")]
     internal class Teleportication : RustPlugin
     {
